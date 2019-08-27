aa6d0f3b40b7d4269ff8d42a9d1065cfb78ab094
Mariusz Sygnowski <sygnowski.m@gmail.com>
2019-08-27 18:18:30 +0100

    fixed issue with input - when the same value from input is exist in any other input, when you edit one of them, it was changed in rest of them

--- src/Chart/SingleMeasure.js
+++ src/Chart/SingleMeasure.js
@@ -1,21 +1,21 @@
 import React, {useState, useEffect, useRef} from 'react';
 
-const SingleMeasure = ({value, handleClickR, newValue, onChangeValue, i}) => {
+const SingleMeasure = ({value, handleClickR, onChangeValue, i}) => {
   const [val, setVal] = useState('');
   useEffect(() => {
     setVal(value);
-  }, [value]);
+  }, []);
   const handleClickRemove = () => {
     handleClickR(value, i);
   };
-  const onChange = (e, i) => {
+  const onChange = e => {
     onChangeValue(value, e.target.value);
     setVal(e.target.value);
   };
 
   return (
     <div>
-      <input value={val} onChange={e => onChange(e, i)} />
+      <input value={val} onChange={e => onChange(e)} />
       <button onClick={handleClickRemove}>click to remove</button>
     </div>
   );
